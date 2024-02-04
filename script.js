4d527da3c8e920dd699e5c689aa3370e3d7c5d09
Rakesh-5199 <kr65921@gmail.com>
2024-02-04 19:28:54 +0530

    Day 5 Task

---
+++ script.js
@@ -0,0 +1,59 @@
+const resume = {
+    "name": "Rakesh.k",
+    "contact": {
+      "email": "Kr65921@gmail.com.com",
+      "phone": "8072502391",
+      "address": "32 vel nagar 5th street kosakulam madurai-2."
+    },
+    "summary": "Experienced and results-driven professional with a strong background in software development. Adept at collaborating with cross-functional teams to deliver high-quality solutions. Proven track record of successful project management and excellent problem-solving skills.",
+    "education": [
+      {
+        "degree": "Bachelor of commerce",
+        "college": "Yadava college",
+        "year": 2018
+      },
+      {
+        "degree": "Master of Bussiness Administration",
+        "college": "Alagppa university",
+        "year": 2021
+      }
+    ],
+    "experience": [
+      {
+        "title": "nil",
+        
+        "responsibilities": [
+          "Develop and maintain web applications using modern technologies.",
+          "Collaborate with cross-functional teams to design and implement software solutions.",
+          "Conduct code reviews and provide mentorship to junior team members.",
+          "Participate in agile development processes to ensure timely project delivery."
+        ]
+      },
+      {
+        "title": "Junior Software Developer",
+       
+        "responsibilities": [
+          "Assisted in the development of software applications.",
+          "Troubleshooted and resolved software defects.",
+          "Collaborated with senior developers to enhance coding skills."
+        ]
+      }
+    ],
+    "skills": [
+      "Programming Languages: JavaScript, Python, Java",
+      "Web Technologies: HTML, CSS, React, Node.js",
+      "Database: MySQL, MongoDB",
+      "Version Control: Git",
+      "Project Management: Agile, Scrum",
+      "Problem Solving: Analytical thinking, Troubleshooting"
+    ],
+    "certifications": [
+      {
+        "title": "Full Stack Developer",
+       
+        "year": 20224
+      },
+     
+    ]
+  };
+  console.log(resume);
